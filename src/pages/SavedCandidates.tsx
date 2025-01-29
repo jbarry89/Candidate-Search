b098db336c8cd003eb3916d10a9415ca99a92027
jbarry89 <joelbarry8907@hotmail.com>
2025-01-29 00:46:51 -0500

    Added code to create table

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import { Candidate } from "../interfaces/Candidate.interface";
+import { unstable_batchedUpdates } from "react-dom";
 
 const SavedCandidates = () => {
-  const [candidates, setCandidates] = useState ([]);
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
 
   useEffect(() => {
-    const SavedCandidates = JSON.parse(localStorage.getItem('savedCandidates')) || [];
-    setCandidates(SavedCandidates);
+    const savedCandidates = JSON.parse(localStorage.getItem('potentialCandidates') || '[]');
+    setCandidates(savedCandidates);
   }, []);
 
   if(!candidates.length){
@@ -14,19 +16,42 @@ const SavedCandidates = () => {
     );
   }
 
+  const handleReject = (index: number) => {
+    const updatedCandidates = candidates.filter((_, i) => i !== index);
+    setCandidates(updatedCandidates);
+    localStorage.setItem('potentialCandidates', JSON.stringify(updatedCandidates));
+
+  };
+
   return (
     <>
       <h1>Potential Candidates</h1>
+      <table className="candidate-table">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Email</th>
+            <th>Company</th>
+            <th>Bio</th>
+            <th>Reject</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
       {candidates.map((candidate, index) => (
-        <div key={index}>
-          <p>Name: {candidate.name}</p>
-          <p>Username: {candidate.username}</p>
-          <p>Location: {candidate.location}</p>
-          <img src={candidate.avatar_url} alt={`${candidate.username}'s avatar`} />
-          <p>Email: {candidate.email}</p>
-          <p>URL: <a href={candidate.html_url}>{candidate.html_url}</a></p>
-          <p>Company: {candidate.company}</p>
-        </div>
+        <tr key={index}>
+          <td><img src={candidate.avatar_url} alt={`${candidate.login}'s avatar`} /></td>
+          <td>{candidate.name} <span>({candidate.login})</span></td>
+          <td>{candidate.location}</td>
+          <td>{candidate.email}</td>
+          <td>{candidate.company}</td>
+          <td>{candidate.bio}</td>
+          <td><button onClick={() => handleReject(index)}>-</button></td>
+        </tr>  
       ))}
     </>
   );
