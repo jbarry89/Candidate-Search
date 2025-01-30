ffdd36d14efdf1cc0ea7555ef8bb43ac0689fe26
jbarry89 <joelbarry8907@hotmail.com>
2025-01-30 18:04:17 -0500

    Modified code to properly execute the .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  envDir: './env',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE'); // Ensure VITE_ prefix is loaded
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env, // Expose env variables
+    },
+  };
 });
